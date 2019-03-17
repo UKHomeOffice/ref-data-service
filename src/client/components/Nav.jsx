9dd864e06c6af02cb7e37276ece6a058b864beca
Pedro Miguel <pedro.miguel@live.co.uk>
2019-03-17 18:23:54 +0000

    Add navigation

---
+++ src/client/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Nav extends React.Component {
+  render() {
+    return (
+      <nav>
+        <ul id="navigation" className="govuk-header__navigation " aria-label="Top Level Navigation">
+          <li className="govuk-header__navigation-item govuk-header__navigation-item--active">
+            <Link className="govuk-header__link" to="/">Home</Link>
+          </li>
+          <li className="govuk-header__navigation-item">
+            <Link className="govuk-header__link" to="#">Search</Link>
+          </li>
+          <li className="govuk-header__navigation-item">
+            <Link className="govuk-header__link" to="#">Signout</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
