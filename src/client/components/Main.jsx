cc6bca2293f59afb9bd92dd56b108d660aa76708
Pedro Miguel <pedro.miguel@live.co.uk>
2019-03-25 11:55:34 +0000

    Update navigation

--- src/client/components/Main.jsx
+++ src/client/components/Main.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 // local imports
+import DataEntities from 'DataEntities';
+import Entities from 'Entities';
+import EntityDetail from 'EntityDetail';
 import Footer from 'Footer';
 import Header from 'Header';
-import Home from 'Home';
 import Search from 'Search';
 
 export default class Main extends React.Component {
@@ -14,8 +16,10 @@ export default class Main extends React.Component {
         <div>
           <a href="#main-content" className="govuk-skip-link">Skip to main content</a>
           <Header/>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/search' component={Search}/>
+          <Route exact path='/' component={DataEntities}/>
+          <Route path='/search' component={Search}/>
+          <Route exact path='/entities/:name' component={Entities}/>
+          <Route path='/entities/:name/items/:id' component={EntityDetail}/>
           <Footer/>
         </div>
       </Router>
