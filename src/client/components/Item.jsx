8daa6d968c067a6269796b6ed1dd30286319343a
Pedro Miguel <pedro.miguel@live.co.uk>
2019-04-05 01:20:34 +0100

    Move item data into its own component

--- src/client/components/Item.jsx
+++ src/client/components/Item.jsx
@@ -6,17 +6,42 @@ import util from 'util';
 import Banner from 'Banner';
 import config from '../../config/core';
 
+const ItemData = ({
+  name,
+  id,
+  itemObject: {
+    data,
+    entitySchema: {
+      properties
+    }
+  }
+}) => {
+  let itemRows = [];
+  let itemFields = Object.entries(properties).forEach(([key, value]) => {
+      let itemChangeField = `/entities/${name}/items/${id}/edit/${key}`;
+      itemRows.push(
+        <div className="govuk-summary-list__row" key={key}>
+          <dt className="govuk-summary-list__key">{value.description.label}</dt>
+          <dd className="govuk-summary-list__value">{data[key]}</dd>
+          <dd className="govuk-summary-list__actions">
+            <Link className="govuk-link" to={itemChangeField}>Change</Link>
+          </dd>
+        </div>
+      )
+    });
+  return itemRows;
+};
+
 export default class Item extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      itemObject: {entitySchema: {properties: {keyId: {description: {} } } }, data: {} },
-      params: props.match.params,
+      itemObject: {},
     };
   }
 
   componentDidMount() {
-    const entityDetailUrl = util.format(config.apiEntityDetailUrl, this.state.params.name, this.state.params.id);
+    const entityDetailUrl = util.format(config.apiEntityDetailUrl, this.props.match.params.name, this.props.match.params.id);
     fetch(entityDetailUrl)
       .then(res => res.json())
       .then(obj => {
@@ -25,22 +50,8 @@ export default class Item extends React.Component {
   }
 
   render() {
-    const name = this.state.params.name;
-    const id = this.state.params.id;
-    const editUrl = `/entities/${name}/items/${id}/edit/`;
-
-    let itemDataRows = [];
-    let itemFields = Object.entries(this.state.itemObject.entitySchema.properties).forEach(([key, value]) => {
-      itemDataRows.push(
-        <div className="govuk-summary-list__row" key={key}>
-          <dt className="govuk-summary-list__key">{value.description.label}</dt>
-          <dd className="govuk-summary-list__value">{this.state.itemObject.data[key]}</dd>
-          <dd className="govuk-summary-list__actions">
-            <Link className="govuk-link" to={editUrl + key}>Change</Link>
-          </dd>
-        </div>
-      )
-    });
+    const {name, id} = this.props.match.params;
+    const itemDelete = `/entities/${name}/items/${id}/delete`;
 
     return (
       <div className="govuk-width-container">
@@ -48,16 +59,18 @@ export default class Item extends React.Component {
         <Link className="govuk-back-link" to={`/entities/${this.state.itemObject.entityName}`}>Back</Link>
         <main className="govuk-main-wrapper " id="main-content" role="main">
           <div className="govuk-grid-row">
-            <div className="govuk-grid-column-two-thirds-from-desktop">
-              <h1 className="govuk-heading-xl">{this.state.itemObject.data.name}</h1>
-              <h2 className="govuk-heading-m">Fields</h2>
-              <p className="govuk-body-l">A list of fields that make up this data item. Click the change link to request changes to individual fields.</p>
-              <dl className="govuk-summary-list govuk-!-margin-bottom-9">
-                {itemDataRows}
-              </dl>
-            </div>
+            {this.state.itemObject && this.state.itemObject.entitySchema &&
+              <div className="govuk-grid-column-two-thirds-from-desktop">
+                <h1 className="govuk-heading-xl">{this.state.itemObject.data.name}</h1>
+                <h2 className="govuk-heading-m">Fields</h2>
+                <p className="govuk-body-l">A list of fields that make up this data item. Click the change link to request changes to individual fields.</p>
+                <dl className="govuk-summary-list govuk-!-margin-bottom-9">
+                  <ItemData name={name} id={id} itemObject={this.state.itemObject} />
+                </dl>
+              </div>
+            }
           </div>
-          <Link className="govuk-button" to={`/entities/${this.state.itemObject.entity}/items/${this.state.itemObject.itemid}/delete`} role="button" draggable="false">Delete data item</Link>
+          <Link className="govuk-button" to={itemDelete} role="button" draggable="false">Delete data item</Link>
         </main>
       </div>
     );
