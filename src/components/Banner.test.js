d2e4a933a4dd7454bf60685d27d5a521d9058b89
Pedro Miguel <pedro.miguel@live.co.uk>
2019-06-14 17:18:08 +0100

    Add unittest for application Banner

---
+++ src/components/Banner.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Banner from './Banner';
+
+describe('Banner', () => {
+  it('Should render banner successfully', () => {
+    const component = shallow(<Banner/>);
+
+    expect(component).toMatchSnapshot();
+  });
+});
