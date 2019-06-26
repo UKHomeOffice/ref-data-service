3405c66f8044d867210735c883b26230c5c3d56f
Pedro Miguel <pedro.miguel@live.co.uk>
2019-06-26 17:06:11 +0100

    Add custom validation styling

--- src/components/ItemNew.jsx
+++ src/components/ItemNew.jsx
@@ -1,13 +1,15 @@
-import { Form, Field } from "react-final-form";
 import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import util from 'util';
+import { Form, FormSpy, Field } from 'react-final-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 // local imports
-import Banner from 'Banner';
+import Banner from './Banner';
+import ErrorSummary from './ErrorSummary';
 import config from '../../config/core';
 import logger from '../../logger';
+import { getTouchedFields, touchedFieldHasValue } from '../utils';
 
 const { appUrls, apiUrls } = config;
 
@@ -19,47 +21,53 @@ const ErrorField = ({ name }) => (
   <Field
     name={name}
     subscription={{ touched: true, error: true }}
-    render={({ meta: { touched, error } }) =>
+    render={({ meta: { touched, error } }) => (
       touched && error ? <span>{error}</span> : null
-    }
+    )}
   />
 )
+
+const InputField = ({ field, fieldProperties, meta: { touched, error} }) => {
+    const idHintStyle = `${field}-hint`;
+    const idErrorStyle = `${field}-error`;
+    const inputWidth = `govuk-input govuk-date-input__input govuk-input--width-${fieldProperties[field].maxLength}`;
+
+    return (
+      <div className={error && touched ? 'govuk-form-group govuk-form-group--error': 'govuk-form-group'}>
+        <label className='govuk-label' htmlFor={field}>{fieldProperties[field].description.label}</label>
+        <span id={idHintStyle} className='govuk-hint'>{fieldProperties[field].description.description}</span>
+        <span id={idErrorStyle} className='govuk-error-message'>
+          <ErrorField name={field} />
+        </span>
+        <Field className={inputWidth} name={field} component='input'/>
+      </div>
+    )
+};
 
 const ItemFields = ({ requiredFields, fieldProperties }) => {
   let fieldElements = [];
 
   for (const field in fieldProperties) {
     if (field !== 'id') {
-      let idHintStyle = `${field}-hint`;
-      let idErrorStyle = `${field}-error`;
-      let inputWidth = `govuk-input govuk-input--width-${fieldProperties[field].maxLength}`;
-
-      const itemFields = (
-        <div className="govuk-form-group">
-          <label className="govuk-label" htmlFor={field}>{fieldProperties[field].description.label}</label>
-          <span id={idHintStyle} className="govuk-hint">{fieldProperties[field].description.description}</span>
-          <span id={idErrorStyle} className="govuk-error-message">
-            <ErrorField className="govuk-visually-hidden" name={field} />
-          </span>
-          <Field className={inputWidth} name={field} component="input"/>
-        </div>
-      );
-
       if (requiredFields.includes(field)) {
         fieldElements.push(
           <Field
             name={field}
+            field={field}
+            fieldProperties={fieldProperties}
             validate={required}
             key={field}
-            render={() => itemFields}
+            component={InputField}
           />
         );
       } else {
         fieldElements.push(
           <Field
             name={field}
+            field={field}
+            fieldProperties={fieldProperties}
             key={field}
-            render={() => itemFields}
+            component={InputField}
           />
         );
       }
@@ -143,32 +151,47 @@ class ItemNew extends React.Component {
 
   render() {
     let fieldProperties, requiredFields;
+    const { data } = this.state;
     const backLink = util.format(appUrls.entity, this.props.match.params.name);
 
-    if (this.state.data && this.state.data.entitySchema) {
-      (fieldProperties = this.state.data.entitySchema.properties);
-      (requiredFields = this.state.data.entitySchema.required);
+    if (data && data.entitySchema) {
+      (fieldProperties = data.entitySchema.properties);
+      (requiredFields = data.entitySchema.required);
     }
 
     return (
-      <div className="govuk-width-container">
+      <div className='govuk-width-container'>
         <Banner/>
-        <Link className="govuk-back-link" to={backLink}>Back</Link>
-        <main className="govuk-main-wrapper " id="main-content" role="main">
-          <div className="govuk-grid-row">
-            <div className="govuk-grid-column-two-thirds">
-              <h1 className="govuk-heading-xl">Add a new data item to the data set</h1>
-              <p className="govuk-body">Please provide the following information. Once this form has been submitted, it will be sent to the data owner selected for approval.</p>
+        <Link className='govuk-back-link' to={backLink}>Back</Link>
+        <main className='govuk-main-wrapper ' id='main-content' role='main'>
+          <div className='govuk-grid-row'>
+            <div className='govuk-grid-column-two-thirds'>
               <Form
                 onSubmit={this.handleSubmit}
-                render={({ handleSubmit, submitting, values }) => (
-                  <form onSubmit={handleSubmit}>
-                    {this.state.data && this.state.data.entitySchema &&
+                render={({ handleSubmit, submitting, values, form }) => {
+                  const touchedFields = getTouchedFields(form.getState().touched);
+                  const hasErrors = touchedFieldHasValue(touchedFields, form.getState().values);
+                  return(
+                    <form onSubmit={handleSubmit}>
+
+                      { hasErrors ?
+                        <div className='govuk-error-summary'>
+                          <h2 class='govuk-error-summary__title' id='error-summary-title'>There is a problem</h2>
+                          <ul className='govuk-list govuk-error-summary__list'>
+                            <ErrorSummary fieldProperties={fieldProperties} />
+                          </ul>
+                        </div>
+                      :
+                        <div className='error-summary'><ul></ul></div>
+                      }
+
+                      <h1 className='govuk-heading-xl'>Add a new data item to the data set</h1>
+                      <p className='govuk-body'>Please provide the following information. Once this form has been submitted, it will be sent to the data owner selected for approval.</p>
                       <ItemFields requiredFields={requiredFields} fieldProperties={fieldProperties} />
-                    }
-                    <button className="govuk-button" type="submit" disabled={submitting}>Submit change for approval</button>
-                  </form>
-                )}
+                      <button className='govuk-button' type='submit' disabled={submitting}>Submit change for approval</button>
+                    </form>
+                  )
+                }}
               />
             </div>
           </div>
--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,38 @@ const getKeyByValue = (obj, value) => {
   return Object.keys(obj).find(key => obj[key] === value);
 }
 
-export default getKeyByValue;
+// takes an object of required fields and
+// returns an array with all `touched` fields
+const getTouchedFields = touchedFieldsObject => {
+  let fields = [];
+
+  if (Object.keys(touchedFieldsObject).length > 0) {
+    for (const key in touchedFieldsObject) {
+      if (touchedFieldsObject[key] === true) {
+        fields.push(key);
+      }
+    }
+  };
+  return fields;
+};
+
+// takes an array of `touched` fields and check
+// if there's a field missing in the values object
+const touchedFieldHasValue = (fields, valuesObject) => {
+  let displayErrorSummary = false;
+
+  if (fields.length > 0) {
+    fields.map(field => {
+      if (!(field in valuesObject)) {
+        displayErrorSummary = true;
+      }
+    })
+  }
+  return displayErrorSummary;
+};
+
+export {
+  getKeyByValue,
+  getTouchedFields,
+  touchedFieldHasValue,
+}
