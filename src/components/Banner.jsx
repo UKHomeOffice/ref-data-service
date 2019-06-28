c78346f076e7504a1bc8c3b9e6120b76a911fcdb
Pedro Miguel <pedro.miguel@live.co.uk>
2019-06-28 10:30:17 +0100

    Update phase banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ export default class Banner extends React.Component {
     return (
       <div className="govuk-phase-banner">
         <p className="govuk-phase-banner__content">
-          <strong className="govuk-tag govuk-phase-banner__content__tag ">alpha</strong>
+          <strong className="govuk-tag govuk-phase-banner__content__tag ">beta</strong>
           <span className="govuk-phase-banner__text">
             This is a new service – your <a className="govuk-link" href="#">feedback</a> will help us to improve it.
           </span>
