57721fbe9805bdf841ecf3759c4310d305051839
Riccardo Coppola <ricca509@gmail.com>
2020-06-09 12:50:32 +0100

    Removes the 'Last Updated' column on the Data Sets page

--- src/components/__tests__/Entities.test.jsx
+++ src/components/__tests__/Entities.test.jsx
@@ -9,22 +9,36 @@ import entitiesObject from '../__fixtures__/entitiesObject';
 const mockStore = configureMockStore();
 
 describe('Entities component', () => {
-  it('Should display the entities component successfully', () => {
+  const keycloakToken = {
+    'keycloak': {
+      'token': '1357',
+      'tokenParsed': {
+        'name': 'Eve Polastri',
+        'email': 'eve.polastri@mail.com',
+      },
+    },
+  };
+  
+  beforeEach(() => {
     global.fetch = jest
       .fn()
       .mockImplementation(() => Promise.resolve({
         'json': () => entitiesObject
-      }));
+      }));    
+  });
 
-    const keycloakToken = {
-      'keycloak': {
-        'token': '1357',
-        'tokenParsed': {
-          'name': 'Eve Polastri',
-          'email': 'eve.polastri@mail.com',
-        },
-      },
-    };
+  it('should render a table with the right data', () => {
+    const store = mockStore(keycloakToken);
+    const wrapper = shallow(<Entities store={ store }/>).dive();
+
+    wrapper.setState({
+      'entitiesObject': entitiesObject
+    });
+
+    expect(wrapper.find('EntitiesData').dive()).toMatchSnapshot();
+  });
+
+  it('Should display the entities component successfully', () => {    
     const store = mockStore(keycloakToken);
     const wrapper = shallow(<Entities store={ store }/>).dive();
 
@@ -37,22 +51,19 @@ describe('Entities component', () => {
     expect(wrapper.containsMatchingElement(<h2 className="govuk-heading-l">Data Sets</h2>)).toBeTruthy();
 
     // table headers
-    expect(wrapper.find('.govuk-table__row').children()).toHaveLength(4);
+    expect(wrapper.find('.govuk-table__row').children()).toHaveLength(3);
     expect(wrapper.find('.govuk-table__header').get(0).props.children).toEqual('Name');
     expect(wrapper.find('.govuk-table__header').get(1).props.children).toEqual('Description');
-    expect(wrapper.find('.govuk-table__header').get(2).props.children).toEqual('Last Updated');
-    expect(wrapper.find('.govuk-table__header').get(3).props.children).toEqual('Action');
+    expect(wrapper.find('.govuk-table__header').get(2).props.children).toEqual('Action');
 
     // table content
-    expect(wrapper.find('EntitiesData').dive().find('.govuk-table__row').children()).toHaveLength(8);
+    expect(wrapper.find('EntitiesData').dive().find('.govuk-table__row').children()).toHaveLength(6);
     for (let i = 0; i < entitiesObject.data.length; i++) {
       expect(wrapper.find('EntitiesData').dive().get(i).props.children[0].props.children).toEqual(entitiesObject.data[i].schema.label);
-      expect(wrapper.find('EntitiesData').dive().get(i).props.children[1].props.children).toEqual(entitiesObject.data[i].schema.description);
-      // debugger;
-      expect(wrapper.find('EntitiesData').dive().get(i).props.children[2].props.children).toEqual(entitiesObject.data[i].schema.schemalastupdated);
-      expect(wrapper.find('EntitiesData').dive().get(i).props.children[3].props.children[0].props.children).toEqual('View');
-      expect(wrapper.find('EntitiesData').dive().get(i).props.children[3].props.children[1].props.children[0].props.children).toEqual('Edit');
-      expect(wrapper.find('EntitiesData').dive().get(i).props.children[3].props.children[1].props.children[1].props.children).toEqual('Delete');
+      expect(wrapper.find('EntitiesData').dive().get(i).props.children[1].props.children).toEqual(entitiesObject.data[i].schema.description);            
+      expect(wrapper.find('EntitiesData').dive().get(i).props.children[2].props.children[0].props.children).toEqual('View');
+      expect(wrapper.find('EntitiesData').dive().get(i).props.children[2].props.children[1].props.children[0].props.children).toEqual('Edit');
+      expect(wrapper.find('EntitiesData').dive().get(i).props.children[2].props.children[1].props.children[1].props.children).toEqual('Delete');
     };
   });
 
